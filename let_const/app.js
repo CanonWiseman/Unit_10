d90432926b26eb4a01bee9b601f0930a7a666daa
canonwiseman <wisemancanon@gmail.com>
2022-05-04 09:08:11 -0500

    added files for let and const exercise

---
+++ let_const/app.js
@@ -0,0 +1,14 @@
+let PI = 3.14159;
+PI = 42;
+
+//or if you didnt want to change PI you could just use a const variable on it.
+
+// 1. with var you can both redeclare and reassign a value but with let you can only reassign a value. Also Let is in a block scope
+
+// 2. Const won't let you reassign or redeclare a value. Const also creates a block scope.
+
+//3. Let will let you reassign values but const will not.
+
+//4. Variable hoisting is the way the compiler takes the undefined value to the top of the page before it is assigned to a value but only works with VAR and not let or const.
+
+
